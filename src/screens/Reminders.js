2577f35276a8f4c80508f46d66eb362390471a58
Maria Contreras <mariafernanda993@gmail.com>
2023-06-25 16:25:14 +0100

    Added Reminder boxes 'Today', 'This Week' and 'This Month'

--- src/screens/Reminders.js
+++ src/screens/Reminders.js
@@ -5,6 +5,20 @@ const Reminders = () => {
     return (
         <View style={StyleSheet.container}>
             <Text style={styles.header}>July, 8th 2023</Text>
+            <View style={styles.reminderBoxesContainer}>
+                <View style={styles.reminderBox}>
+                    <Text style={styles.reminderBoxTitle}>Today</Text>
+                    {/* TODO: Add the number of reminders for today */}
+                </View>
+                <View style={styles.reminderBox}>
+                    <Text style={styles.reminderBoxTitle}>This Week</Text>
+                    {/* TODO: Add the number of reminders for this week */}
+                </View>
+                <View style={styles.reminderBox}>
+                    <Text style={styles.reminderBoxTitle}>This Month</Text>
+                    {/* TODO: Add the number of reminders for this month */}
+                </View>
+            </View>
         </View>
     );
 };
@@ -22,7 +36,26 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         marginBottom: 10,
         color: 'purple',
-    }
+    },
+    reminderBoxesContainer: {
+        flexDirection:'row',
+        justifyContent: 'space-evenly',
+        marginBottom: 10,
+    },
+    reminderBox: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'purple',
+        borderRadius: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      reminderBoxTitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'white',
+      },
+      
 });
 
 export default Reminders;
