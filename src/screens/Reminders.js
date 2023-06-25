386834e346ee26712276bfd76b2a6795266e40cf
Maria Contreras <mariafernanda993@gmail.com>
2023-06-25 16:05:11 +0100

    Add a new file 'Reminders.js' in the 'screens' folder and implemented the basic structure of the Reminders screen. Updated 'App.js' to include the Reminders screen in the navigation stack

---
+++ src/screens/Reminders.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Reminders = () => {
+    return (
+        <View style={StyleSheet.container}>
+            {/* TODO: Add the screen content */}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'lavender',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+});
+
+export default Reminders;
