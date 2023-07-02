a5faacbad6c231c7673881e46b51e38627ff1d30
Maria Contreras <mariafernanda993@gmail.com>
2023-07-02 10:16:40 +0100

    Deleted the index.js file as it caused an error. Direclty imported authReducer to the App.js file

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomePage from './src/screens/Home';
@@ -5,20 +7,24 @@ import EventsPage from './src/screens/Events';
 import TasksPage from './src/screens/Tasks';
 import Reminders from './src/screens/Reminders';
 import Login from './src/screens/Login';
-import styles from './src/styles/AppStyles'
+import styles from './src/styles/AppStyles';
+import authReducer from './src/reducers/authReducer';
 
 const Stack = createStackNavigator();
+const store = createStore(authReducer);
 
 export default function App() {
   return (
-    <NavigationContainer style={styles.container}>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Home" component={HomePage} />
-        <Stack.Screen name="Events" component={EventsPage} />
-        <Stack.Screen name="Tasks" component={TasksPage} />
-        <Stack.Screen name="Reminders" component={Reminders} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer style={styles.container}>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Home" component={HomePage} />
+          <Stack.Screen name="Events" component={EventsPage} />
+          <Stack.Screen name="Tasks" component={TasksPage} />
+          <Stack.Screen name="Reminders" component={Reminders} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
