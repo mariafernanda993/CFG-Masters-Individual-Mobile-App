7fc6663927cf79cf35ae2657cbb6f5763511e343
Maria Contreras <mariafernanda993@gmail.com>
2023-07-02 14:43:07 +0100

    Added the username to the top right corner of the Tasks screen

--- src/screens/Tasks.js
+++ src/screens/Tasks.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, Alert } from 'react-native';
 import { CheckBox } from 'react-native-elements';
+import { useSelector } from 'react-redux';
 import AppStyles from '../styles/AppStyles';
 
 const TasksPage = () => {
+  const username = useSelector(state => state.username); // Fetch the username
   const [checked, setChecked] = useState([false, false, false, false, false]);
 
   const tasks = [
@@ -25,14 +27,17 @@ const TasksPage = () => {
 
   return (
     <View style={AppStyles.container}>
-      <Text style={AppStyles.header}>This Week:</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Text style={AppStyles.header}>This Week:</Text>
+        <Text style={AppStyles.header}>Hi, {username || 'Guest'}</Text> 
+      </View>
       {tasks.map((task, index) => (
         <View key={index} style={AppStyles.taskContainer}>
           <CheckBox
-            checked={checked[index]}
-            onPress={() => handleCheck(index)}
-            checkedColor='purple'
-            uncheckedColor='purple'
+          checked={checked[index]}
+          onPress={() => handleCheck(index)}
+          checkedColor='purple'
+          uncheckedColor='purple'
           />
           <Text style={AppStyles.taskText}>{task}</Text>
         </View>
